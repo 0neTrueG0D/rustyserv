271d718b8bc644a1d8de022167520cc623b23230
zzedddd <melegend.forever@protonmail.com>
2022-09-08 09:32:10 -0530

    Refactor TCP listener and add request handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
@@ -6,10 +9,25 @@ fn main() {
         match stream {
             Ok(stream) => {
                 println!("New connection: {}", stream.peer_addr().unwrap());
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("Error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    println!("Request: {:#?}", http_request);
+    stream.write_all(response.as_bytes()).unwrap();
 }
