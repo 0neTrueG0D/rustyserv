68f866d5b3771e1ad30268ebc54d0113f5de1836
zzedddd <melegend.forever@protonmail.com>
2022-09-07 19:32:10 -0530

    create basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
 }
